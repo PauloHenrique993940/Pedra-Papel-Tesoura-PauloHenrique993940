5bfcea643754001e291dc0893c6d22c50be20bbe
Paulo Henrique Ferreira França <162806228+PauloHenrique993940@users.noreply.github.com>
2024-04-20 14:09:01 -0300

    Jogo Concluído

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 /* Desenvolva seu código abaixo */
-console.log("Primeira Entrega")
+/* Criando Projeto  */
+
+
+function playRockPaperScissor(player1, player2){
+    var pedra = "Pedra";
+    var papel = "Papel";
+    var tesoura = "Tesoura";
+
+    // Verificar se há empate
+    if (player1 === player2) {
+        return "Empate!";
+    }
+    
+    // Verificar as combinações onde player1 vence
+    if ((player1 === pedra && player2 === tesoura) ||
+        (player1 === papel && player2 === pedra) ||
+        (player1 === tesoura && player2 === papel)) {
+        return "Jogador 1 venceu!";
+    }
+
+    // Se não houve empate e player1 não venceu, então player2 vence
+    return "Jogador 2 venceu!";
+}
+
+// Solicitando as escolhas dos jogadores player1 e Player2 e  chamando a função com essas escolhas
+var escolhaPlayer1 = prompt("Jogador 1, escolha: Pedra, Papel ou Tesoura?");
+var escolhaPlayer2 = prompt("Jogador 2, escolha: Pedra, Papel ou Tesoura?");
+alert(playRockPaperScissor(escolhaPlayer1, escolhaPlayer2));
+
